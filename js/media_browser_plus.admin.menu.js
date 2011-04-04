2dd486957c60b60546c385855714c0675e12e215
James Elliott <james.elliott@acquia.com>
2011-04-04 16:26:19 -0400

    Adding latest from github

--- js/media_browser_plus.admin.menu.js
+++ js/media_browser_plus.admin.menu.js
@@ -2,14 +2,22 @@
   Drupal.behaviors.media_browser_menu = {
     attach: function (context) {
       // append links
+      if($('ul.action-links').html() == null) {
+        $('#media-admin').prepend('<div><ul class="action-links"></ul></div>');
+      }
       var $filterLink = "";
-      if(Drupal.settings.media_browser_plus.filter_active)
-        $filterLink = $('<a href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Change <b>(active)</b> Filter');
+      if(Drupal.settings.media_browser_plus.filter_active && Drupal.settings.media_browser_plus.filter_allowed)
+        $filterLink = $('<a id="media-filter-launch" href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Change <b>(active)</b> Filter');
       else
-        $filterLink = $('<a href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Apply Filter');
+        $filterLink = $('<a id="media-filter-launch" href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/filter"></a>').html('Apply Filter');
       $('ul.action-links', context).append($('<li></li>').append($filterLink));
-      var $settingsLink = $('<a href="' + Drupal.settings.media_browser_plus.url + '?q=admin/content/media/settings"></a>').html('Media Settings');
-      $('ul.action-links', context).append($('<li></li>').append($settingsLink));
+      if(Drupal.settings.media_browser_plus.manage_folders) {
+        var $foldersLink = $('<a href="' + Drupal.settings.media_browser_plus.folder_management_url + '"></a>').html('Folder Management');
+        $('ul.action-links', context).append($('<li></li>').append($foldersLink));
+      }
+      if(!Drupal.settings.media_browser_plus.add_files) {
+        $('.media-launcher').remove();
+      }
     }
   }
 })(jQuery);
