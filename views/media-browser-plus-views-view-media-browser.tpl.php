a7d1e55031cde4d3881685f809c6969291776f23
camdarley <camdarley@954720.no-reply.drupal.org>
2013-05-21 08:01:11 -0700

    Issue #954720 by camdarley: Table layout for view template

--- views/media-browser-plus-views-view-media-browser.tpl.php
+++ views/media-browser-plus-views-view-media-browser.tpl.php
@@ -9,21 +9,27 @@
  */
 ?>
 <?php print $wrapper_prefix; ?>
-<div class="clearfix">
-<?php if(!empty($folders)) : ?>
-<div class="mbp-folders">
-  <?php print $folders ?>
-</div>
-<?php endif; ?>
-  <div class="mbp-file-list">
-    <?php print $list_type_prefix; ?>
-      <?php foreach ($rows as $id => $row): ?>
-        <li id="media-item-<?php print $view->result[$id]->fid; ?>" class="<?php print $classes_array[$id]; ?>">
-          <?php print $row; ?>
-        </li>
-      <?php endforeach; ?>
-    <?php print $list_type_suffix; ?>
-    <div id="status"></div>
-  </div>
-</div>
+<table>
+  <thead>
+    <tr>
+      <?php if(!empty($folders)) : ?><th width="200"><?php print t('Folders') ?></th><?php endif; ?>
+      <th width="*"><?php print t('Media Files') ?></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <?php if(!empty($folders)) : ?><td class="mbp-folders"><?php print $folders ?></td><?php endif; ?>
+      <td class="mbp-file-list">
+        <?php print $list_type_prefix; ?>
+        <?php foreach ($rows as $id => $row): ?>
+          <li id="media-item-<?php print $view->result[$id]->fid; ?>" class="<?php print $classes_array[$id]; ?>">
+            <?php print $row; ?>
+          </li>
+        <?php endforeach; ?>
+        <?php print $list_type_suffix; ?>
+        <div id="status"></div>
+      </td>
+    </tr>
+  </tbody>
+</table>
 <?php print $wrapper_suffix; ?>
