dc230ab1bb2bcbdaca7a5c234687970c8fcf88cc
Peter Philipp <peter.philipp@cando-image.com>
2014-07-10 08:37:20 +0200

    Minor usability enhancements by adding destination parameters.

--- js/media_browser_plus.js
+++ js/media_browser_plus.js
@@ -185,7 +185,7 @@
         window.setTimeout(function(){
           $('.mbp-file-basket-list li', plugin.element).trigger('click');
         }, 1000);
-        window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/download-multiple/' + $('input[name=mbp_basket_files]', plugin.element).val();
+        window.location.href = this.getBasePath() + 'admin/content/file/download-multiple/' + $('input[name=mbp_basket_files]', plugin.element).val();
       });
 
       if (this.options.files_draggable) {
@@ -269,6 +269,25 @@
     $('.mbp-action-', this.element).unbind('.mbp');
   };
 
+  /**
+   * Returns the proper Drupal path to use as destination parameter.
+   */
+  MBP.prototype.getDestinationPath = function () {
+    var path = window.location.pathname;
+    path = path.replace(new RegExp('^' + Drupal.settings.basePath.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1')), '');
+    if (Drupal.settings.pathPrefix.length) {
+      path = path.replace(new RegExp('^' + Drupal.settings.pathPrefix.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1')), '');
+    }
+    return path;
+  };
+
+  /**
+   * Returns the proper Drupal path to navigate with.
+   */
+  MBP.prototype.getBasePath = function (path) {
+    return Drupal.settings.basePath + Drupal.settings.pathPrefix;
+  };
+
   MBP.prototype.folderOpen = function(folder) {
     $(folder)
       .addClass('open')
@@ -304,7 +323,7 @@
   MBP.prototype.deleteFiles = function () {
     var fids = this.getSelectedFiles();
     if (fids.length) {
-      window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/delete-multiple/' + fids.join(' ');
+      window.location.href = this.getBasePath() + 'admin/content/file/delete-multiple/' + fids.join(' ') + '?destination=' + this.getDestinationPath();
     }
   }
 
@@ -324,17 +343,17 @@
       fids = [fileId];
     }
     if (fids.length > 1) {
-      window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/edit-multiple/' + fids.join(' ');
+      window.location.href = this.getBasePath()+ 'admin/content/file/edit-multiple/' + fids.join(' ') + '?destination=' + this.getDestinationPath();
     }
     else if (fids.length == 1) {
-      window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'file/' + fids[0] + '/edit';
+      window.location.href = this.getBasePath() + 'file/' + fids[0] + '/edit?destination=' + this.getDestinationPath();
     }
   }
 
   MBP.prototype.downloadFiles = function () {
     var fids = this.getSelectedFiles();
     if (fids.length) {
-      window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/download-multiple/' + fids.join(' ');
+      window.location.href = this.getBasePath() + 'admin/content/file/download-multiple/' + fids.join(' ');
     }
   }
 
